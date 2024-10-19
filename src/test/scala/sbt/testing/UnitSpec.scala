b58675d09240e85ff11f5c93a8e99976df85f24a
xuwei-k <6b656e6a69@gmail.com>
2024-10-19 09:56:44 +0900

    update dependencies

--- src/test/scala/sbt/testing/UnitSpec.scala
+++ src/test/scala/sbt/testing/UnitSpec.scala
@@ -1,7 +1,9 @@
 package sbt.testing
 
-import org.scalatest._
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.prop.TableDrivenPropertyChecks
+import org.scalatest.refspec.RefSpec
 
-class UnitSpec extends Spec with Matchers with prop.TableDrivenPropertyChecks {
+class UnitSpec extends RefSpec with Matchers with TableDrivenPropertyChecks {
   def red(): String = "der".reverse
 }
